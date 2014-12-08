258f0cfb919279d5640a60d709fc059cd996df3c
liwenjun <wenjun.lee.job@gmail.com>
2014-12-08 11:34:10 +0800

    nbi

---
+++ algorithm/nbi.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 08 10:35:42 2014
+
+@author: Administrator
+"""
+import numpy as np
+import scipy as sp
+
+def gen_ajacent_from_txt(file,sep=None):
+    data=np.genfromtxt(file,delimiter=sep)
+    return sp.sparse.coo_matrix((data[:,2],(data[:,0]-1,data[:,1]-1))).toarray()
+
+def nbi(mat):
+    sum_u = np.sum(mat,axis=1,keepdims=True)
+    sum_o = np.sum(mat,axis=0,keepdims=True)
+    
+    tmp=mat/sum_u
+    
+    t=np.dot(tmp.T,mat)/sum_o
+    
+    p=np.dot(mat,t)
+    
+if __name__=='__main__':
+    file = 'G:\\projects_lwj\\data\\ml-1m\\ratings.dat'
